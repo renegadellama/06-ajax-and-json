fb493d44cddb73c8fe5ccec9125ea811afef20ec
Isak Swearingen <isakswearingen@gmail.com>
2017-03-21 17:08:36 -0700

    got local storage stringified

--- starter-code/scripts/article.js
+++ starter-code/scripts/article.js
@@ -50,9 +50,17 @@ Article.fetchAll = function() {
     // When rawData is already in localStorage,
     // we can load it with the .loadAll function above,
     // and then render the index page (using the proper method on the articleView object).
-    Article.loadAll(?); //TODO: What do we need to pass in to Article.loadAll()?
+    Article.loadAll(Article.all); //TODO: What do we need to pass in to Article.loadAll()?
     //TODO: What method do we call to render the index page?
   } else {
+    $.getJSON('/data/hackerIpsum.json').then(function(data){
+      var rawData = JSON.stringify(data)
+      // localStorage.setItem(rawData)
+      console.log(data);
+      // console.log(storage);
+      console.log(rawData);
+    });
+
     // TODO: When we don't already have the rawData,
     // we need to retrieve the JSON file from the server with AJAX (which jQuery method is best for this?),
     // cache it in localStorage so we can skip the server call next time,
